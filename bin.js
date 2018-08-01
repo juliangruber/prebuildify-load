c54ce140f5801e8e507405f492e0300226045da3
Julian Gruber <julian@juliangruber.com>
2018-08-01 09:55:13 +0200

    fix bin name

--- bin.js
+++ bin.js
@@ -2,7 +2,7 @@
 
 var proc = require('child_process')
 
-proc.exec('node-gyp-build-test', function (err) {
+proc.exec('prebuildify-load-test', function (err) {
   if (err) process.exit(1)
   process.exit(0)
 })
